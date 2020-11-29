c7d6f2bd9badd347bc03bb0b1ef530af44037b5a
qzp332 <qzp332@bupt.edu.cn>
2020-11-29 21:01:35 +0800

    Train_model

---
+++ NN.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+import csv
+
+input_x = []
+f1 = csv.reader(open('TrainSamples.csv','r'))
+for index, i in enumerate(f1):
+    temp_x = []
+    for elm in range(len(i)):
+        temp_x.append(int(float(i[elm])))
+    input_x.insert(index, temp_x)
+    if(index < 10):
+        print(temp_x)
+
+print("-----------")
+
+output_y = []
+
+inputs = tf.keras.Input(shape=(84,))
+x = tf.keras.layers.Dense(169, use_bias=True, activation='sigmoid')(inputs)
+outputs = tf.keras.layers.Dense(1, use_bias=True, activation='sigmoid')(x)
+m = tf.keras.Model(inputs, outputs)     # 使用 输入 和 输出 创建模型
+
+m.compile(tf.keras.optimizers.SGD(learning_rate=0.1), 'mse')
+m.fit(input_x, output_y, epochs=1000, batch_size=1, verbose=0)
+
+# 保存模型
+m.save('widow_model.h5')
---
+++ Restore_model.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+import csv
+
+test_case = []
+f1 = csv.reader(open('TrainSamples.csv','r'))
+for index, i in enumerate(f1):
+    temp_x = []
+    for elm in range(len(i)):
+        temp_x.append(int(float(i[elm])))
+    test_case.insert(index, temp_x)
+    if(index < 10):
+        print(temp_x)
+
+output_y = []
+
+# 恢复模型
+restored_model = tf.keras.models.load_model('widow_model.h5')
+
+for i in range(len(test_case)):
+    temp_case = np.array([test_case[i]])
+    print("Case" + str(i) + ": " + str(restored_model.predict(temp_case)) )
